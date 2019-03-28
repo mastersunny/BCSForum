cabd6db656760edd1c8e0a0275fe60523fa28005
sunnychowdhury <sunny@dginnovationlab.com>
2019-03-28 14:02:59 +0600

    sms verification added

--- app/src/main/java/com/inflack/bcsforum/SplashActivity2.java
+++ app/src/main/java/com/inflack/bcsforum/SplashActivity2.java
@@ -4,6 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -21,6 +27,65 @@ public class SplashActivity2 extends AppCompatActivity {
 
     @OnClick({R.id.btn_login})
     public void onClick(View v) {
+        phoneLogin();
+    }
+
+    public static int APP_REQUEST_CODE = 99;
+
+    public void phoneLogin() {
+        final Intent intent = new Intent(this, AccountKitActivity.class);
+        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(
+                        LoginType.PHONE,
+                        AccountKitActivity.ResponseType.CODE); // or .ResponseType.TOKEN
+        // ... perform additional configuration ...
+        intent.putExtra(
+                AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,
+                configurationBuilder.build());
+        startActivityForResult(intent, APP_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(
+            final int requestCode,
+            final int resultCode,
+            final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == APP_REQUEST_CODE) { // confirm that this response matches your request
+            AccountKitLoginResult loginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            String toastMessage;
+            if (loginResult.getError() != null) {
+                toastMessage = loginResult.getError().getErrorType().getMessage();
+            } else if (loginResult.wasCancelled()) {
+                toastMessage = "Login Cancelled";
+            } else {
+                toastMessage = "Login Success";
+//                if (loginResult.getAccessToken() != null) {
+//                    toastMessage = "Success:" + loginResult.getAccessToken().getAccountId();
+//                } else {
+//                    toastMessage = String.format(
+//                            "Success:%s...",
+//                            loginResult.getAuthorizationCode().substring(0, 10));
+//                }
+
+                // If you have an authorization code, retrieve it from
+                // loginResult.getAuthorizationCode()
+                // and pass it to your server and exchange it for an access token.
+
+                // Success! Start your next activity...
+                goToMyLoggedInActivity();
+            }
+
+            // Surface the result to your user in an appropriate way.
+            Toast.makeText(
+                    this,
+                    toastMessage,
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    private void goToMyLoggedInActivity() {
         Intent intent = new Intent(SplashActivity2.this, MainActivity.class);
         startActivity(intent);
         finish();
