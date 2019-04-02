80fe61f184835ff415ae9e062d5e745a71f524a1
sunnychowdhury <sunny@dginnovationlab.com>
2019-04-02 16:27:48 +0600

    edit profile done

--- app/src/main/java/com/inflack/bcsforum/EditProfileActivity.java
+++ app/src/main/java/com/inflack/bcsforum/EditProfileActivity.java
@@ -1,21 +1,33 @@
 package com.inflack.bcsforum;
 
+import android.Manifest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.facebook.accountkit.AccountKit;
+import com.ornach.bitpermission.BitPermission;
+import com.ornach.bitpermission.PermissionListener;
+
+import java.io.InputStream;
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import es.dmoral.toasty.Toasty;
 
 public class EditProfileActivity extends AppCompatActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    private final int PICK_PHOTO_FOR_AVATAR = 999;
+    private final int UPDATE_PROFILE_INFO = 888;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +53,7 @@ public class EditProfileActivity extends AppCompatActivity {
         return true;
     }
 
-    @OnClick({R.id.btn_logout, R.id.img_notification})
+    @OnClick({R.id.btn_logout, R.id.img_notification, R.id.img_choose_photo, R.id.img_edit_name, R.id.img_edit_desig})
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_logout:
@@ -52,10 +64,74 @@ public class EditProfileActivity extends AppCompatActivity {
                 finish();
                 break;
             case R.id.img_choose_photo:
+                requestPermission();
                 break;
             case R.id.img_notification:
                 Utils.startNotificationActivity(EditProfileActivity.this);
                 break;
+            case R.id.img_edit_name:
+                intent = new Intent(EditProfileActivity.this, EditProfileActivity2.class);
+                intent.putExtra(EditProfileActivity2.NAME, "মোহাম্মদ আব্দুল হাই, পি এ এ");
+                startActivityForResult(intent, UPDATE_PROFILE_INFO);
+                break;
+            case R.id.img_edit_desig:
+                intent = new Intent(EditProfileActivity.this, EditProfileActivity2.class);
+                intent.putExtra(EditProfileActivity2.DESIGNATION, "প্রকল্প পরিচালক (ডেপুটি সেক্রেটারি)");
+                intent.putExtra(EditProfileActivity2.COMPANY, "স্কিল ডেভেলপমেন্ট ফর মোবাইল গেইম এন্ড এপ্লিকেশন প্রজেক্ট");
+                startActivityForResult(intent, UPDATE_PROFILE_INFO);
+                break;
+        }
+    }
+
+    private void requestPermission() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add(Manifest.permission.CAMERA);
+        list.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+
+        BitPermission bitPermission = BitPermission.with(this)
+                .setPermissionListener(listener)
+                .setPermissions(list)
+                .build();
+        bitPermission.request();
+    }
+
+    PermissionListener listener = new PermissionListener() {
+        @Override
+        public void onPermissionGranted() {
+            pickImage();
+        }
+
+        @Override
+        public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+
+        }
+    };
+
+    public void pickImage() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_PHOTO_FOR_AVATAR);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_PHOTO_FOR_AVATAR && resultCode == RESULT_OK) {
+            if (data == null) {
+                return;
+            }
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(data.getData());
+            } catch (Exception e) {
+
+            }
+            //Now you can do whatever you want with your inpustream, save it as file, upload to a server, decode a bitmap...
+        }
+
+        if (requestCode == UPDATE_PROFILE_INFO && requestCode == RESULT_OK) {
+//            Toasty.success(EditProfileActivity.this, "Profile updated successfully!", Toast.LENGTH_SHORT, true).show();
+//            Toast.makeText(EditProfileActivity.this, "Profile updated successfully!",Toast.LENGTH_LONG).show();
         }
     }
 }
---
+++ app/src/main/java/com/inflack/bcsforum/EditProfileActivity2.java
@@ -0,0 +1,109 @@
+package com.inflack.bcsforum;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.inflack.bcsforum.model.MemberDTO;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import es.dmoral.toasty.Toasty;
+
+public class EditProfileActivity2 extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.edt_name)
+    TextView edt_name;
+
+    @BindView(R.id.edt_designation)
+    TextView edt_designation;
+
+    @BindView(R.id.edt_company)
+    TextView edt_company;
+
+    @BindView(R.id.edt_name_layout)
+    LinearLayout edt_name_layout;
+
+    @BindView(R.id.edt_designation_layout)
+    LinearLayout edt_designation_layout;
+
+    @BindView(R.id.edt_company_layout)
+    LinearLayout edt_company_layout;
+
+    private MemberDTO memberDTO = new MemberDTO();
+
+    public static final String NAME = "name";
+    public static final String DESIGNATION = "designation";
+    public static final String COMPANY = "company";
+
+    private String name, designation, company;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile2);
+
+        ButterKnife.bind(this);
+
+        getIntentData();
+        initLayout();
+    }
+
+    private void getIntentData() {
+        Intent intent = getIntent();
+        if (intent.hasExtra(NAME)) {
+            edt_name.setText(intent.getStringExtra(NAME));
+            edt_name_layout.setVisibility(View.VISIBLE);
+            edt_designation_layout.setVisibility(View.GONE);
+            edt_company_layout.setVisibility(View.GONE);
+        } else {
+            edt_name_layout.setVisibility(View.GONE);
+            edt_designation_layout.setVisibility(View.VISIBLE);
+            edt_company_layout.setVisibility(View.VISIBLE);
+            if (intent.hasExtra(DESIGNATION)) {
+                edt_designation.setText(intent.getStringExtra(DESIGNATION));
+            }
+            if (intent.hasExtra(COMPANY)) {
+                edt_company.setText(intent.getStringExtra(COMPANY));
+            }
+        }
+    }
+
+    private void initLayout() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("প্রোফাইল");
+        toolbar.setNavigationIcon(R.drawable.ic_arrow);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @OnClick({R.id.btn_update})
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_update:
+                Intent intent = new Intent();
+                intent.putExtra("OK", "");
+                setResult(RESULT_OK, intent);
+                Toasty.success(EditProfileActivity2.this, "Profile updated successfully!", Toast.LENGTH_SHORT, true).show();
+                finish();
+                break;
+        }
+
+    }
+}
