7c2c3cbb7bd15145ea22bed9def5ce906b7bca1c
sunnychowdhury <sunny@dginnovationlab.com>
2019-04-08 15:05:27 +0600

    edit profile done

--- app/src/main/java/com/inflack/bcsforum/MainActivity.java
+++ app/src/main/java/com/inflack/bcsforum/MainActivity.java
@@ -83,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MemberListActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(intent);
             }
         });
